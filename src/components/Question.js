63fc3e08a0087a573bd32562027c83193495e22a
MonsterBasket <blazza1@hotmail.com>
2022-08-26 22:04:10 +1000

    a nice easy one, unlike the stupid pigs

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const timerID = setTimeout(()=> {
+      setTimeRemaining(timeRemaining -1)
+      if (timeRemaining === 1){ // set this =to 1 rather than 0 because it still takes 1 second to trigger.  Apparently...
+        onAnswered(false);
+        setTimeRemaining(10)
+      }
+    }, 1000);
+
+    return function cleanup() {
+      clearInterval(timerID);
+    }
+  }, [timeRemaining])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
